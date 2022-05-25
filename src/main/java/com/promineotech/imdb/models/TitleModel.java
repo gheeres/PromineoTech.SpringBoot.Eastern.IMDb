56b4cfed6f81697a7481385c7f8aa047bebcc037
George Heeres <gheeres@gmail.com>
2022-05-24 19:51:42 -0500

    Week 2

---
+++ src/main/java/com/promineotech/imdb/models/TitleModel.java
@@ -0,0 +1,33 @@
+package com.promineotech.imdb.models;
+
+public class TitleModel {
+  private String id;
+  private String name;
+  private int releasedYear;
+  
+  public TitleModel(String id, String name, int releasedYear) {
+    setId(id);
+    setName(name);
+    setReleasedYear(releasedYear);
+  }
+  
+  public String getId() {
+    return id;
+  }
+  public void setId(String id) {
+    this.id = id;
+  }
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public int getReleasedYear() {
+    return releasedYear;
+  }
+  public void setReleasedYear(int releasedYear) {
+    this.releasedYear = releasedYear;
+  }
+  
+}
